ef40e00140b4f5c3bd0ad2b35b138887e091e4e8
Filip Vesely <vesel.fil@gmail.com>
2024-09-14 21:17:54 +0200

    chore: remove old migration scritp

--- init_tables.sh
+++
@@ -1,8 +0,0 @@
-for file in $(find ./migs -name '*.sql' -type f); do
-    {
-        printf %s\\n 'BEGIN TRANSACTION;'
-        cat "$file"
-        printf %s\\n 'COMMIT;'
-    } | sqlite3 "fuck-idealni-najemnik.sqlite" 2> "migrationerr.log"
- 
-done
